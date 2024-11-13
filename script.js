c77fe4642ce25aecdc018fcb53dd2e56076dd199
dannyperezz104 <152096769+dannyperezz104@users.noreply.github.com>
2024-11-13 15:34:50 -0500

    add computer choice function

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hello, World!")
+function getComputerChoice(){
+    let choiceNum = Math.floor(Math.random()*3)+1;
+    if (choiceNum == 1){
+        return "rock";
+    }
+    else if (choiceNum == 2){
+        return "paper";
+    } else{
+        return "scissors";
+    }
+}
+
+console.log(getComputerChoice());
