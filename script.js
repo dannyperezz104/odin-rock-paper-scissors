a1f39542c1f0121c181a1bec3f49ee1dc1f3a7fa
dannyperezz104 <152096769+dannyperezz104@users.noreply.github.com>
2024-11-13 16:23:18 -0500

    add playRound function

--- script.js
+++ script.js
@@ -10,4 +10,54 @@ function getComputerChoice(){
     }
 }
 
-console.log(getComputerChoice());
+function getHumanChoice(){
+    let choice = prompt('Please enter "rock", "paper", or "scissors"');
+    return choice;
+}
+
+let computerScore = 0;
+let humanScore = 0;
+
+function playRound(humanChoice, computerChoice){
+    humanChoice = humanChoice.toLowerCase();
+
+    if (humanChoice == "rock"){
+        if (computerChoice == "rock"){
+            console.log("Computer chose Rock. You tied!");
+        }else if (computerChoice == "paper"){
+            console.log("You lose! Paper beats Rock");
+            computerScore++;
+        } else {
+            console.log("You win! Rock beats Scissors");
+            humanScore++;
+        }
+    }
+    else if (humanChoice == "paper"){
+        if (computerChoice == "rock"){
+            console.log("You win! Paper beats Rock.");
+            humanScore++;
+        }else if (computerChoice == "paper"){
+            console.log("Computer chose Paper. You tied!");
+        } else {
+            console.log("You lose! Scissors beats Paper");
+            computerScore++;
+        }
+    }
+    else {
+        if (computerChoice == "rock"){
+            console.log("You lose! Rock beats Scissors.");
+            computerScore++;
+        }else if (computerChoice == "paper"){
+            console.log("You win! Scissors beats Paper.");
+            humanScore++;
+        } else {
+            console.log("Computer chose Scissors. You tied!");
+            computerScore++;
+        }
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
